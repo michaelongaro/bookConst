2888552eb66547ff6c07985b485d535193a1bfb8
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2021-02-18 19:57:10 -0600

    added styling + basic cards

--- scripts.js
+++ scripts.js
@@ -11,6 +11,7 @@ function Book(title, author, pages, have_read) {
 }
 
 // submit button on the form will call this function i guess
+// will also need to remove '.introText' class/text visibility once book is added
 function addBookToLibrary(title, author, pages, have_read) {
   myLibrary.append(new Book(title, author, pages, have_read));
   console.log(myLibrary);
@@ -25,4 +26,58 @@ Book.prototype.changeRead = function() {
   this.have_read = true;
 }
 
-document.getElementById('newBookForm').style.display = 'block';
+document.getElementById('button').addEventListener('click', function() {
+  document.querySelector('.bg-modal').style.display = 'flex';
+})
+
+document.querySelector('.close').addEventListener('click', function () {
+  document.querySelector('.bg-modal').style.display = 'none';
+})
+
+document.getElementById('newBookForm').onsubmit = function() { 
+  let a = document.getElementById('title').value;
+  let b = document.getElementById('author').value;
+  let c = document.getElementById('pagelength').value;
+  let d = document.getElementById('finishedreading').value;
+
+  addBookToLibrary(a, b, c, d);
+  console.log("i have been run!");
+  return false;
+};
+
+
+let bodyElement = document.body;
+//The document.createElement() method create html elements specified by tagName
+let cardElement = document.createElement('div');
+let imageContainer = document.createElement('div');
+let infoContainer = document.createElement('div');
+let imageElement = document.createElement('img');
+let headingElement = document.createElement('h5');
+let paragraphElement = document.createElement('p');
+let btnElement = document.createElement('a');
+
+//The ClassName property gets and sets the value of the class attribute of the spefified element
+cardElement.className = "card";
+imageContainer.className = "image-container";
+infoContainer.className = "info-container";
+imageElement.className = "image";
+headingElement.className = "heading";
+paragraphElement.className = "paragraph";
+btnElement.className = "btn";
+
+//Works the same way as the className property except it sets the source of the imageElement
+imageElement.src = "https://source.unsplash.com/random";
+
+// This sets the value of an attribute specified element. If exists then will be updated, otherwise the new attribute is added with the specified name and value
+btnElement.setAttribute("href", "#");
+imageElement.setAttribute("alt", "Image from Unsplash");
+
+headingElement.innerText = "Unsplash API";
+paragraphElement.innerText = "This sets the value of an attribute specified element. If exists then will be updated.";
+btnElement.innerText = "Learn more";
+
+bodyElement.appendChild(cardElement);
+cardElement.append(imageContainer, infoContainer);
+
+imageContainer.appendChild(imageElement);
+infoContainer.append(headingElement, paragraphElement, btnElement);
